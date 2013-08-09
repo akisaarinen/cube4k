12cc1c1e5026e07601ffa0ffe61dfff4497356af
Aki Saarinen <aki@akisaarinen.fi>
2013-08-10 01:04:47 +0300

    Configure grunt to minify JavaScript
    Signed-off-by: Aki Saarinen <aki@akisaarinen.fi>

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['js/*.js'],
+        dest: 'dist/app.js'
+      }
+    },
+    uglify: {
+      options: {
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['concat', 'uglify']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['concat', 'uglify']);
+
+};
